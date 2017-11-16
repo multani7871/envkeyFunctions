01452625a0003b06062d1a493e3101e16e2b95b0
Harvey Multani <harvey.multani@gmail.com>
2017-11-15 21:41:52 -0800

    Trying a bunch of things

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,10 @@
-require('envkey');
 const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+console.log(process.env.TESTENV);
+require('envkey');
 
 exports.sendEnvironment = functions.https.onRequest((request, response) => {
     response.header('Access-Control-Allow-Origin', '*');
-    response.end(process.env.TESTENV);
+    console.log(process.env.SOME);
+    const enviro = process.env.TESTENV;
+    response.end(enviro);
 })
