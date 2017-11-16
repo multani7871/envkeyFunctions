0ac2e0271baaa265005cc55d2bbf05ccf949e193
Harvey Multani <harvey.multani@gmail.com>
2017-11-15 19:37:49 -0800

    initial

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+require('envkey');
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.sendEnvironment = functions.https.onRequest((request, response) => {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.end(process.env.TESTENV);
+})
